94fd87d14e3105824081a15e6a08d0fadb2a93f2
pro-shiv <shivamtikkha@gmail.com>
2023-03-11 21:11:46 +0530

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(errorHandler);
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
