6049fb8c7f0ba36317300522c5507398b4f93f66
pro-shiv <shivamtikkha@gmail.com>
2023-03-18 20:51:29 +0530

    initial commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     const connect = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log(
       "Database connected: ",
-      connect.connect.host,
+      connect.connection.host,
       connect.connection.name
     );
   } catch (err) {
